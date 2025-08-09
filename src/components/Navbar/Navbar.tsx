557f51f9960f4c4ca456a9b735bdaf058c3d199b
Aswin <aswinvsivan@gmail.com>
2025-08-09 22:11:29 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -30,7 +30,7 @@ function Navbar() {
                         <>
                             <span>aswinvsivan</span>
                             <img
-                                src="public\envar-studio-valorant-first-breakfast-final-02-22.jpg"
+                                src="/envar-studio-valorant-first-breakfast-final-02-22.jpg"
                                 alt="User Avatar"
                                 className={styles.avatarImage}
                             />
